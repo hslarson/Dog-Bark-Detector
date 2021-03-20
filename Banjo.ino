a8414f95f15a738ada505b372c1c1f47468363c6
Hunter Larson <hslarson@umail.ucsb.edu>
2021-03-19 20:58:54 -0700

    Fully Functioning

--- Banjo.ino
+++ Banjo.ino
@@ -1,38 +1,41 @@
 #include <ThingSpeak.h>
 #include <ESP8266WiFi.h>
-#include "constants.h" // Not included because it contains passwords & stuff
+#include "constants.h" // Not Included Because it Contains Passwords & Stuff
 
-// Declare functions & global variables
+
+// Declare Functions & Global Variables
 WiFiClient client;
+
 int bark_count = 0;
-bool barking = false;
+bool barking = false, tone_enabled = false;
 
 void sendToServer(int, int);
+bool connectToWifi();
 bool timer();
 
 
 void setup() {
-    // Start Serial Monitor
-    Serial.begin(115200);
-    
-    // Connect to Wifi
-    Serial.print("Connecting to ");
-    Serial.println(SSID);
- 
-    WiFi.begin(SSID, PASSWORD);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+    if (DEBUG) {
+        Serial.begin(115200);
+        delay(1000);
     }
-
-    Serial.println("\nWiFi connected");
+    connectToWiFi();
 
     // Start ThingSpeak
-    ThingSpeak.begin( client );
+    WiFi.mode(WIFI_STA); 
+    ThingSpeak.begin(client);
+
+    // Read Settings Field
+    tone_enabled = (ThingSpeak.readIntField(CHANNEL, 2, READ_API_KEY) == 1 ? true : false);
+    if (DEBUG) {
+        Serial.print("Buzzer Status: ");
+        Serial.println((tone_enabled ? "ON" : "OFF"));
+    }
 }
 
+
 void loop() {
-    // Ping the timer
+    // Ping the Timer
     if (timer()) {
         sendToServer(1, bark_count);
         bark_count = 0;
@@ -40,39 +43,87 @@ void loop() {
 
     // Read Microphone Data
     if (analogRead(MIC_PIN) > VOLUME_THRESHOLD && !barking) {
-        Serial.println("Bark Detected!");
+        if (DEBUG) {Serial.println("Bark Detected!");};
         barking = true;
         bark_count++;
+
+        // Play the Tone
+        if (tone_enabled)
+            tone(BUZZER_PIN, BUZZER_FREQUENCY, BUZZER_LINGERING_TIME * 1000);
     }
 
-    // Only Count Each Bark Once
-    while (barking) {
+    // Wait Until the Bark is Finished
+    int retries = RETRY_ATTEMPTS;
+    while (barking && retries > 0) {
         if (analogRead(MIC_PIN) < VOLUME_THRESHOLD)
-            barking = false;
+            retries--;
+        else
+            retries = RETRY_ATTEMPTS;
+        delay(1);
     }
+    barking = false;
 }
+
 
 // Write Data to the Channel
 void sendToServer(int field, int data) {
-    if (ThingSpeak.writeField(CHANNEL, field, data, API_KEY)) {
-        Serial.print("Sent Data: \"");
-        Serial.print(barks);
-        Serial.print(" Barks\" to Field: ");
-        Serial.print(field)
-        Serial.println("");
-    }
-    else
-        Serial.println("")
-    return;
+  if (DEBUG) {
+    Serial.print("Sending \"");
+    Serial.print(data);
+    Serial.print("\" to Field: ");
+    Serial.println(field);
+  }
+
+  int response = -1;
+  if (connectToWiFi())
+      response = ThingSpeak.writeField(CHANNEL, field, data, WRITE_API_KEY);
+    
+  if (DEBUG) {
+      if(response == -1)
+          Serial.println("Data Not Sent, WiFi Not Connected");
+
+      else {
+          Serial.print("Data Sent With Code: ");
+          Serial.println(response);
+      }
+  }
+  return;
 }
 
-// Signal if it's Time to Send Data to ThingSpeak
-bool timer() {
-    static long start = milis();
 
-    if ( abs(milis() - start) / (SEND_INTERVAL * 1000) ) {
-        start = milis();
+// Attempts to Connect to WiFi, Returns True if Connected
+bool connectToWiFi() {
+    if (WiFi.status() == WL_CONNECTED)
+        return true;
+
+    long timer = millis();
+    if (DEBUG) {
+        Serial.print("Connecting to ");
+        Serial.println(WIFI_NAME);
+    }
+ 
+    WiFi.begin(WIFI_NAME, PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        if (DEBUG) {Serial.print(".");};
+
+        if ( abs(millis() - timer) / (WIFI_TIMEOUT * 1000) )
+          return false;
+    }
+    
+    if (DEBUG) {Serial.println("\nWiFi connected");};
+    return true;
+}
+
+
+// Return True if it's Time to Send Data to ThingSpeak
+bool timer() {
+    static long bark_start = millis();
+
+    if ( abs(millis() - bark_start) / (BARK_SEND_INTERVAL * 1000) ) {
+        bark_start = millis();
         return true;
     }
+    
     return false;
 }
--- constants_template.h
+++ constants_template.h
@@ -2,16 +2,24 @@
 #define constants
 
 
-#define API_KEY "Your Write API Key"
+#define DEBUG true
+
+#define READ_API_KEY "Your Read API Key"
+#define WRITE_API_KEY "Your Write API Key"
 #define CHANNEL 0123456
 
 #define SSID "Your SSID"
 #define PASSWORD "Your Password"
+#define WIFI_TIMEOUT 60 // Seconds
 
 #define MIC_PIN A0
-#define VOLUME_THRESHOLD 600
+#define VOLUME_THRESHOLD 700
+#define RETRY_ATTEMPTS 50
+#define BARK_SEND_INTERVAL 300  // Seconds
 
-#define SEND_INTERVAL 300 // Seconds
+#define BUZZER_PIN D5
+#define BUZZER_FREQUENCY 19000
+#define BUZZER_LINGERING_TIME 5 // Seconds
 
 
 #endif

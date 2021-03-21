ee7a0f37ee28a75997a7288aea981c7a50638004
Hunter Larson <hslarson@umail.ucsb.edu>
2021-03-21 14:37:40 -0700

    Added FFT

--- Banjo.ino
+++ Banjo.ino
@@ -1,81 +1,246 @@
 #include <ThingSpeak.h>
+#include <arduinoFFT.h>
 #include <ESP8266WiFi.h>
 #include "constants.h" // Not Included Because it Contains Passwords & Stuff
 
 
 // Declare Functions & Global Variables
 WiFiClient client;
+arduinoFFT FFT = arduinoFFT();
 
 int bark_count = 0;
-bool barking = false, tone_enabled = false;
+bool barking = false;
 
+unsigned int sampling_period_us;
+unsigned long microseconds;
+double vReal[SAMPLES];
+double vImag[SAMPLES];
+
+int  timer();
+bool isBarking();
+double getAudioLevel();
+bool connectToNetwork();
+bool checkToneEnabled(bool);
 void sendToServer(int, int);
-bool connectToWifi();
-bool timer();
 
 
 void setup() {
     if (DEBUG) {
         Serial.begin(115200);
-        delay(1000);
+        while(!Serial);
     }
-    connectToWiFi();
+    connectToNetwork();
+
+    // Set PinModes
+    pinMode(MIC_PIN, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUZZER_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
 
     // Start ThingSpeak
-    WiFi.mode(WIFI_STA); 
     ThingSpeak.begin(client);
+    checkToneEnabled(true);
 
-    // Read Settings Field
-    tone_enabled = (ThingSpeak.readIntField(CHANNEL, 2, READ_API_KEY) == 1 ? true : false);
-    if (DEBUG) {
-        Serial.print("Buzzer Status: ");
-        Serial.println((tone_enabled ? "ON" : "OFF"));
-    }
+    // Calculate Sampling Period
+    sampling_period_us = round(1000000*(1.0 / SAMPLING_FREQUENCY));
 }
 
 
 void loop() {
     // Ping the Timer
-    if (timer()) {
-        sendToServer(1, bark_count);
-        bark_count = 0;
+    switch (timer()) {
+        case 0:
+            sendToServer(1, bark_count);
+            bark_count = 0;
+            break;
+        case 1:
+            checkToneEnabled(true);
+            break;
+    };
+
+    // Check Microphone Volume
+    if (getAudioLevel() > BARK_THRESHOLD) {
+        if (DEBUG) { 
+            Serial.println("\nBark Detected!");
+            digitalWrite(LED_PIN, LOW); // Active Low
+        }
+
+        // Check if the Noise is a Bark
+        barking = isBarking();
+        
+        if (DEBUG) {
+            Serial.println((barking ? "Bark Confirmed" : "Not a Bark"));
+            digitalWrite(LED_PIN, HIGH);
+        }
     }
 
-    // Read Microphone Data
-    if (analogRead(MIC_PIN) > VOLUME_THRESHOLD && !barking) {
-        if (DEBUG) {Serial.println("Bark Detected!");};
-        barking = true;
-        bark_count++;
+    // When a Bark is Detected...
+    if (barking) {
+        // Update Counters / Flags
+        barking = false;
+        bark_count++; 
 
-        // Play the Tone
-        if (tone_enabled)
+        // Play Tone 
+        if (checkToneEnabled(false))
             tone(BUZZER_PIN, BUZZER_FREQUENCY, BUZZER_LINGERING_TIME * 1000);
     }
+}
 
+
+// Return True if it's Time to Send Data to ThingSpeak
+int timer() {
+    static unsigned long bark_start = millis();
+    static unsigned long tone_enabled_check = millis();
+
+    if ( abs(millis() - bark_start) / (BARK_SEND_INTERVAL * 1000) ) {
+        bark_start = millis();
+        return 0;
+    }
+    if ( abs(millis() - tone_enabled_check) / (TONE_CHECK_INTERVAL * 1000 * 60) ) {
+        tone_enabled_check = millis();
+        return 1; 
+    }
+    
+    return -1;
+}
+
+
+// Returns True if a Valid Bark is Detected
+// Barks Must Meet Duration and Frequency Standards
+bool isBarking() {
+    unsigned long bark_timer = millis();  // Start a Timer
+  
+    // Collect Samples
+    microseconds = micros();
+    for (int i = 0; i < SAMPLES; i++) {
+        vReal[i] = analogRead(MIC_PIN);
+        vImag[i] = 0;
+        while(micros() - microseconds < sampling_period_us);
+        microseconds += sampling_period_us;
+    }
+  
+    // Weigh data
+    FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+  
+    // Compute FFT
+    FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+  
+    // Compute magnitudes
+    FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+
+    // Find Dominant Frequency
+    double x = FFT.MajorPeak(vReal, SAMPLES / 2, SAMPLING_FREQUENCY);
+    if (DEBUG) {
+        Serial.print(" - Dominant Frequency: ");
+        Serial.print(x);
+        Serial.println("Hz");
+    }
+    
     // Wait Until the Bark is Finished
-    int retries = RETRY_ATTEMPTS;
-    while (barking && retries > 0) {
-        if (analogRead(MIC_PIN) < VOLUME_THRESHOLD)
-            retries--;
-        else
-            retries = RETRY_ATTEMPTS;
+    double level = getAudioLevel();
+    while (level > NOISE_FLOOR) {
+        level *= 0.8;
+        level += 0.2 * getAudioLevel();
+        if (DEBUG) {Serial.println(level);}
         delay(1);
     }
-    barking = false;
+
+    // Check the Timer
+    unsigned long bark_duration = abs(millis() - bark_timer);
+    if (DEBUG) {
+        Serial.print(" - Bark Duartion: ");
+        Serial.print(bark_duration);
+        Serial.println("ms");
+    }
+    
+    // Return True if the Bark is Valid
+    return ((bark_duration >= BARK_DURATION_MIN && bark_duration <= BARK_DURATION_MAX) && (x >= BARK_FREQ_MIN && x <= BARK_FREQ_MAX));
+}
+
+
+// Returns the Standard Deviation of a Set of Sampled Audio Data
+double getAudioLevel() {
+    unsigned short readings[VOLUME_SAMPLES];
+    unsigned int period = round(1000000*(1.0 / VOLUME_SAMPLING_FREQUENCY));
+    unsigned long sum_readings = 0, differences = 0;
+    unsigned long timer = micros();
+
+    // Sanple Audio Data
+    for (int i = 0; i < VOLUME_SAMPLES; i++) {
+        readings[i] = analogRead(MIC_PIN);
+        sum_readings += readings[i];
+
+        while(micros() - timer < period);
+        timer += period;
+    }
+
+    // Compute Mean
+    double mean = (double)sum_readings / VOLUME_SAMPLES;
+
+    // Compute Sum of Squared Differences
+    for (unsigned short reading : readings) {
+        differences += pow((double)reading - mean, 2);
+    }
+
+    // Return Standard Deviation
+    return sqrt((double)differences / VOLUME_SAMPLES);
+}
+
+
+// Attempts to Connect to WiFi, Returns True if Connected
+bool connectToNetwork() {
+    if (WiFi.status() == WL_CONNECTED)
+        return true;
+
+    unsigned long timer = millis();
+    if (DEBUG) {
+        Serial.print("Connecting to ");
+        Serial.println(WIFI_NAME);
+    }
+ 
+    WiFi.begin(WIFI_NAME, PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        if (DEBUG) {Serial.print(".");}
+
+        if ( abs(millis() - timer) / (WIFI_TIMEOUT * 1000) )
+          return false;
+    }
+    
+    if (DEBUG) {Serial.println("\nWiFi connected");}
+    return true;
+}
+
+
+// Pulls Data from ThingSpeak to See if the Buzzer is Enabled
+bool checkToneEnabled(bool check) {
+        static bool tone_enabled = false;
+
+        if (!check || !connectToNetwork())
+            return tone_enabled;
+            
+        tone_enabled = (ThingSpeak.readIntField(CHANNEL, 2, READ_API_KEY) == 1 ? true : false);
+        
+        if (DEBUG) {
+            Serial.print("Buzzer Status: ");
+            Serial.println((tone_enabled ? "ENABLED" : "DISABLED"));
+        }
+
+        return tone_enabled;
 }
 
 
 // Write Data to the Channel
 void sendToServer(int field, int data) {
   if (DEBUG) {
-    Serial.print("Sending \"");
-    Serial.print(data);
-    Serial.print("\" to Field: ");
-    Serial.println(field);
+      Serial.print("\nSending \"");
+      Serial.print(data);
+      Serial.print("\" to Field: ");
+      Serial.println(field);
   }
 
   int response = -1;
-  if (connectToWiFi())
+  if (connectToNetwork())
       response = ThingSpeak.writeField(CHANNEL, field, data, WRITE_API_KEY);
     
   if (DEBUG) {
@@ -88,42 +253,4 @@ void sendToServer(int field, int data) {
       }
   }
   return;
-}
-
-
-// Attempts to Connect to WiFi, Returns True if Connected
-bool connectToWiFi() {
-    if (WiFi.status() == WL_CONNECTED)
-        return true;
-
-    long timer = millis();
-    if (DEBUG) {
-        Serial.print("Connecting to ");
-        Serial.println(WIFI_NAME);
-    }
- 
-    WiFi.begin(WIFI_NAME, PASSWORD);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        if (DEBUG) {Serial.print(".");};
-
-        if ( abs(millis() - timer) / (WIFI_TIMEOUT * 1000) )
-          return false;
-    }
-    
-    if (DEBUG) {Serial.println("\nWiFi connected");};
-    return true;
-}
-
-
-// Return True if it's Time to Send Data to ThingSpeak
-bool timer() {
-    static long bark_start = millis();
-
-    if ( abs(millis() - bark_start) / (BARK_SEND_INTERVAL * 1000) ) {
-        bark_start = millis();
-        return true;
-    }
-    
-    return false;
 }
--- constants_template.h
+++ constants_template.h
@@ -8,18 +8,33 @@
 #define WRITE_API_KEY "Your Write API Key"
 #define CHANNEL 0123456
 
-#define SSID "Your SSID"
+#define WIFI_NAME "Your SSID"
 #define PASSWORD "Your Password"
 #define WIFI_TIMEOUT 60 // Seconds
 
 #define MIC_PIN A0
-#define VOLUME_THRESHOLD 700
-#define RETRY_ATTEMPTS 50
-#define BARK_SEND_INTERVAL 300  // Seconds
-
+#define LED_PIN D4
 #define BUZZER_PIN D5
-#define BUZZER_FREQUENCY 19000
+
+#define NOISE_FLOOR 15
+#define BARK_THRESHOLD 50
+#define VOLUME_SAMPLES 32
+#define VOLUME_SAMPLING_FREQUENCY 6400 // Hz
+
+#define BARK_SEND_INTERVAL 300 // Seconds
+#define TONE_CHECK_INTERVAL 15 // Minutes
+
+#define BUZZER_FREQUENCY 12000 // Hz
 #define BUZZER_LINGERING_TIME 5 // Seconds
+
+#define SAMPLES 256  // This Value MUST ALWAYS be a Power of 2
+#define SAMPLING_FREQUENCY (double)8000 // Hz, must be less than 10,000 due to ADC
+
+#define BARK_FREQ_MIN 300 // Hz
+#define BARK_FREQ_MAX 600 // Hz
+#define RETRY_ATTEMPTS 10
+#define BARK_DURATION_MIN 60  // Milliseconds
+#define BARK_DURATION_MAX 250 // Milliseconds
 
 
 #endif
